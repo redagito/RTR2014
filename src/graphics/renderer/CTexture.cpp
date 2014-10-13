25f8349ac0ef97507a598bbd0923903f8df21d1e
agito <agitodev@gmail.com>
2014-10-13 22:08:21 +0200

    Fixed texture bugs

--- src/graphics/renderer/CTexture.cpp
+++ src/graphics/renderer/CTexture.cpp
@@ -1,59 +1,110 @@
 #include "CTexture.h"
 
+#include <cassert>
+#include <string>
+
 #include "debug/RendererDebug.h"
 #include "debug/Log.h"
 
-#include <cassert>
-
-CTexture::CTexture(const std::vector<unsigned char>& image, unsigned int width, unsigned int height,
-                   EColorFormat format, unsigned int mipmapLevel)
-    : m_valid(false), m_textureId(0), m_width(width), m_height(height), m_format(format)
+CTexture::CTexture()
+    : m_valid(false), m_textureId(0), m_width(0), m_height(0), m_format(EColorFormat::Invalid)
 {
-    // Init texture with data
-    if (!init(image, width, height, format, mipmapLevel))
-	{ 
-		LOG_ERROR("Failed to initialize texture.");
-	}
+    // empty
 }
 
-CTexture::~CTexture() { glDeleteTextures(1, &m_textureId); }
+CTexture::CTexture(const std::vector<unsigned char>& image, unsigned int width, unsigned int height,
+                   EColorFormat format, bool createMipmaps)
+    : m_valid(false), m_textureId(0), m_width(0), m_height(0), m_format(format)
+{
+    // Init texture with data
+    if (!init(image, width, height, format, createMipmaps))
+    {
+        LOG_ERROR("Failed to initialize texture.");
+    }
+}
+
+CTexture::CTexture(unsigned int width, unsigned int height, EColorFormat format, bool createMipmaps)
+    : m_valid(false), m_textureId(0), m_width(0), m_height(0), m_format(format)
+{
+    // Init texture with data
+    if (!init({}, width, height, format, createMipmaps))
+    {
+        LOG_ERROR("Failed to initialize texture.");
+    }
+}
+
+CTexture::~CTexture()
+{
+    if (m_valid)
+    {
+        glDeleteTextures(1, &m_textureId);
+    }
+}
 
 bool CTexture::init(const std::vector<unsigned char>& image, unsigned int width,
-                    unsigned int height, EColorFormat format, unsigned int mipmapLevel)
+                    unsigned int height, EColorFormat format, bool createMipmaps)
 {
-	// Error check
-	std::string error;
-	if (hasGLError(error))
-	{
-		LOG_ERROR("GL Error: %s", error.c_str());
-	}
+    // Set format
+    GLint internalFormat;
+    switch (format)
+    {
+    case EColorFormat::GreyScale8:
+        internalFormat = GL_R8;
+        break;
+    case EColorFormat::RGB24:
+        internalFormat = GL_RGB8;
+        break;
+    case EColorFormat::RGBA32:
+        internalFormat = GL_RGBA8;
+        break;
+    default:
+        return false;
+        break;
+    }
+    return init(image, width, height, internalFormat, createMipmaps);
+}
 
+bool CTexture::init(unsigned int width, unsigned int height, GLint format)
+{
+    return init({}, width, height, format, false);
+}
+
+GLuint CTexture::getId() const { return m_textureId; }
+
+bool CTexture::isValid() const { return m_valid; }
+
+void CTexture::setActive(GLint textureUnit) const
+{
+    assert(isValid());
+    glActiveTexture(GL_TEXTURE0 + textureUnit);
+    glBindTexture(GL_TEXTURE_2D, m_textureId);
+}
+
+bool CTexture::init(const std::vector<unsigned char>& image, unsigned int width,
+                    unsigned int height, GLint format, bool createMipmaps)
+{
     // Sanity checks
-    if (width == 0 || height == 0 || format == EColorFormat::Invalid)
+    if (width == 0 || height == 0)
     {
         return false;
     }
 
     // Set format
     unsigned int bytePerPixel = 0;
-    GLint internalFormat;
-	GLenum externalFormat;
+    GLenum externalFormat;
     switch (format)
     {
-    case EColorFormat::GreyScale8:
-        internalFormat = GL_R8;
-		externalFormat = GL_RED;
+    case GL_R8:
+        externalFormat = GL_RED;
         bytePerPixel = 1;
         break;
-    case EColorFormat::RGB24:
-        internalFormat = GL_RGB8;
-		externalFormat = GL_RGB;
+    case GL_RGB8:
+        externalFormat = GL_RGB;
         bytePerPixel = 3;
         break;
-    case EColorFormat::RGBA32:
-		internalFormat = GL_RGBA8;
-		externalFormat = GL_RGBA;
-		bytePerPixel = 4;
+    case GL_RGBA8:
+        externalFormat = GL_RGBA;
+        bytePerPixel = 4;
         break;
     default:
         return false;
@@ -71,43 +122,57 @@ bool CTexture::init(const std::vector<unsigned char>& image, unsigned int width,
     glGenTextures(1, &textureId);
     glBindTexture(GL_TEXTURE_2D, textureId);
 
-	if (hasGLError(error))
-	{
-		LOG_ERROR("GL Error: %s", error.c_str());
-	}
-
     // TODO Filter should be based on arguments and mip map level
     // Set filters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    if (width == 1 && height == 1)
+    {
+        // Special case 1x1 texture
+        // Needs nearest filtering otherwise texture is black?
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    }
+    else
+    {
+        // Default value is linear filtering
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    }
+
     // Set wrap mode
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
 
-	if (hasGLError(error))
-	{
-		LOG_ERROR("GL Error: %s", error.c_str());
-	}
-
     // Load data
-    glTexImage2D(GL_TEXTURE_2D, mipmapLevel, internalFormat, m_width, m_height, 0, externalFormat,
-                 GL_UNSIGNED_BYTE, image.data());
-    
-	if (hasGLError(error))
-	{
-		LOG_ERROR("GL Error: %s", error.c_str());
-	}
+    if (image.empty())
+    {
+        // No image data, only allocate texture space
+        // TODO As of OpenGL 4.3, glTexStorage should be used for this.
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, externalFormat, GL_UNSIGNED_BYTE,
+                     nullptr);
+    }
+    else
+    {
+        // Load image data
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, externalFormat, GL_UNSIGNED_BYTE,
+                     image.data());
+    }
 
-	// Mipmaps
-    glGenerateMipmap(GL_TEXTURE_2D);
-	
-	if (hasGLError(error))
-	{
-		LOG_ERROR("GL Error: %s", error.c_str());
-	}
+    // Mipmaps
+    // Do not generate for 1x1 images
+    if (width != 1 && height != 1 && createMipmaps)
+    {
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
 
     // Unbind
     glBindTexture(GL_TEXTURE_2D, 0);
+
+    std::string error;
+    if (hasGLError(error))
+    {
+        LOG_ERROR("GL Error: %s", error.c_str());
+        return false;
+    }
 
     // Clean up previously created id
     if (m_textureId != 0)
@@ -117,22 +182,8 @@ bool CTexture::init(const std::vector<unsigned char>& image, unsigned int width,
 
     // Set new texture id
     m_textureId = textureId;
+    m_width = width;
+    m_height = height;
     m_valid = true;
-	
-	if (hasGLError(error))
-	{
-		LOG_ERROR("GL Error: %s", error.c_str());
-	}
     return true;
-}
-
-GLuint CTexture::getId() const { return m_textureId; }
-
-bool CTexture::isValid() const { return m_valid; }
-
-void CTexture::setActive(GLint textureUnit) const
-{
-	assert(isValid());
-	glActiveTexture(GL_TEXTURE0 + textureUnit);
-	glBindTexture(GL_TEXTURE_2D, m_textureId);
 }
--- src/graphics/renderer/CTexture.h
+++ src/graphics/renderer/CTexture.h
@@ -11,8 +11,19 @@
 class CTexture
 {
    public:
+    CTexture();
+
+    /**
+     * \brief Create from image data.
+     */
     CTexture(const std::vector<unsigned char>& imageData, unsigned int width, unsigned int height,
-             EColorFormat format, unsigned int mipmapLevel = 0);
+             EColorFormat format, bool createMipmaps = true);
+
+    /**
+     * \brief Creates empty texture.
+     */
+    CTexture(unsigned int width, unsigned int height, EColorFormat format,
+             bool createMipmaps = true);
 
     ~CTexture();
 
@@ -20,22 +31,28 @@ class CTexture
     * \brief Initializes texture data.
     */
     bool init(const std::vector<unsigned char>& imageData, unsigned int width, unsigned int height,
-              EColorFormat format, unsigned int mipmapLevel = 0);
+              EColorFormat format, bool createMipmaps = true);
+
+    bool init(unsigned int width, unsigned int height, GLint format);
 
     /**
     * \brief Returns texture id.
     */
     GLuint getId() const;
 
-	/**
-	* \brief Returns texture validity.
-	*/
-	bool isValid() const;
+    /**
+    * \brief Returns texture validity.
+    */
+    bool isValid() const;
 
-	/**
-	* \bref Sets texture active as texture unit.
-	*/
-	void setActive(GLint textureUnit) const;
+    /**
+    * \bref Sets texture active as texture unit.
+    */
+    void setActive(GLint textureUnit) const;
+
+   protected:
+    bool init(const std::vector<unsigned char>& imageData, unsigned int width, unsigned int height,
+              GLint format, bool createMipmaps);
 
    private:
     bool m_valid;
