748fc4ce24ee97b76bed9347224ead9578115b8f
Marko Mlinaric <markomlinaric1989@gmail.com>
2015-01-17 17:49:49 +0100

    normal mapping, better scene

--- data/shadersource/deferred/geometry_pass_fragment.glsl
+++ data/shadersource/deferred/geometry_pass_fragment.glsl
@@ -1,6 +1,7 @@
 #version 330 core
 
 in vec2 uv;
+smooth in vec3 vertexWorldSpace;
 smooth in vec3 normalVectorWorldSpace;
 
 // Material textures
@@ -17,6 +18,33 @@ layout(location = 0) out vec4 diffuse_glow;
 // Normals and specular value
 layout(location = 1) out vec4 normal_specular;
 
+mat3 cotangent_frame( vec3 N, vec3 p, vec2 uv )
+{
+    // get edge vectors of the pixel triangle
+    vec3 dp1 = dFdx( p );
+    vec3 dp2 = dFdy( p );
+    vec2 duv1 = dFdx( uv );
+    vec2 duv2 = dFdy( uv );
+    
+    // solve the linear system
+    vec3 dp2perp = cross( dp2, N );
+    vec3 dp1perp = cross( N, dp1 );
+    vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+    vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+    
+    // construct a scale-invariant frame
+    float invmax = inversesqrt( max( dot(T,T), dot(B,B) ) );
+    return mat3( T * invmax, B * invmax, N );
+}
+
+vec3 perturb_normal( vec3 N, vec3 V, vec2 texcoord )
+{
+    vec3 map = texture( normal_texture, texcoord ).xyz;
+    map = map * 255./127. - 128./127.;
+    mat3 TBN = cotangent_frame( N, -V, texcoord );
+    return normalize( TBN * map );
+}
+
 void main(void)
 {
 	vec3 color = texture(diffuse_texture, uv).rgb;
@@ -31,6 +59,6 @@ void main(void)
 	diffuse_glow.a = glow;
 	
 	// normal.rgb = textureNormal;
-    normal_specular.rgb = normalVectorWorldSpace;
+    normal_specular.rgb = perturb_normal(normalVectorWorldSpace, vertexWorldSpace, uv);
 	normal_specular.a = specular;
 }
